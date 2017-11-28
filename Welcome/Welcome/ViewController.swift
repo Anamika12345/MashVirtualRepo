c5e48f52ec8524bffeb83649b7d8a3207929a309
Admin <Admin@AnamikaMacPro.local>
2017-11-28 17:53:11 +0530

    Commit Welcome

---
+++ Welcome/Welcome/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Welcome
+//
+//  Created by Admin on 28/11/17.
+//  Copyright © 2017 Amit Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Welcome in Delhi/NCR")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
